c4e8ffe3d79ea0818cdb1eb930fefbdba55133f5
Steve Mask <smask@paypal.com>
2022-06-22 16:29:04 -0400

    test authorize scenario

--- test/client/inlinexo.js
+++ test/client/inlinexo.js
@@ -226,6 +226,95 @@ describe('Inline XO cases', () => {
         });
     });
 
+    it('should call onComplete if experience is inline and get order when captured', async () => {
+        return await wrapPromise(async ({ expect, avoid }) => {
+
+            const orderID = generateOrderID();
+            const facilitatorAccessToken = uniqueID();
+
+            window.xprops.experience = 'inline';
+
+            window.xprops.createOrder = mockAsyncProp(expect('createOrder', async () => {
+                return ZalgoPromise.try(() => {
+                    return orderID;
+                });
+            }));
+
+            window.xprops.onCancel = avoid('onCancel');
+            window.xprops.onApprove = avoid('onApprove');
+            window.xprops.onError = avoid('onError');
+
+            window.xprops.onComplete = expect('onComplete', async (data, actions) => {
+                if (data.orderID !== orderID) {
+                    throw new Error(`Expected orderID to be ${ orderID }, got ${ data.orderID }`);
+                }
+
+                if (!actions.redirect) {
+                    throw new Error(`Expected actions.redirect() to be available.`);
+                }
+
+                const getOrderMock = getRestfulCaptureOrderApiMock({
+                    handler: expect('getOrder', ({ headers }) => {
+                        if (headers.authorization !== `Bearer ${ facilitatorAccessToken }`) {
+                            throw new Error(`Expected call to come with correct facilitator access token`);
+                        }
+
+                        return {
+                            id: orderID
+                        };
+                    })
+                });
+                getOrderMock.expectCalls();
+                await actions.get();
+                getOrderMock.done();
+            });
+
+            mockFunction(window.paypal, 'Checkout', expect('Checkout', ({ original: CheckoutOriginal, args: [ props ] }) => {
+
+                mockFunction(props, 'onComplete', expect('onComplete', ({ original: onCompleteOriginal, args: [ data, actions ] }) => {
+                    return onCompleteOriginal({ ...data }, actions);
+                }));
+
+                const checkoutInstance = CheckoutOriginal(props);
+
+                mockFunction(checkoutInstance, 'renderTo', expect('renderTo', async ({ original: renderToOriginal, args }) => {
+                    const [ win, element, context ] = args;
+
+                    if (!win) {
+                        throw new Error(`Expected window to be passed to renderTo`);
+                    }
+
+                    if (!element || typeof element !== 'string') {
+                        throw new Error(`Expected string element to be passed to renderTo`);
+                    }
+
+                    if (context !== 'iframe') {
+                        throw new Error(`Expected context to be iframe, got ${ context }`);
+                    }
+
+                    return props.createOrder().then(id => {
+                        if (id !== orderID) {
+                            throw new Error(`Expected orderID to be ${ orderID }, got ${ id }`);
+                        }
+
+                        return renderToOriginal(...args);
+                    });
+                }));
+
+                return checkoutInstance;
+            }));
+
+            createButtonHTML({ fundingEligibility });
+            await mockSetupButton({
+                experience: 'inline',
+                merchantID: [ 'XYZ12345' ],
+                fundingEligibility,
+                facilitatorAccessToken
+            });
+            await clickButton(FUNDING.CARD);
+        });
+    });
+
     it('should not fail when calling onComplete if experience is inline and onComplete callback is not provided', async () => {
         return await wrapPromise(async ({ expect, avoid }) => {
 
